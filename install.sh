33ee4c2e41a20aee3448cd7351e93e7a2473fc98
Jason Berlinsky <jberlinsky@gmail.com>
2025-01-31 04:50:28 +0000

    Disable changes to apt sources

--- install.sh
+++ install.sh
@@ -11,36 +11,36 @@
 clear
 
 if [ -e /root/OSX-PROXMOX ]; then rm -rf /root/OSX-PROXMOX; fi;
-if [ -e /etc/apt/sources.list.d/pve-enterprise.list ]; then rm -rf /etc/apt/sources.list.d/pve-enterprise.list; fi;
-if [ -e /etc/apt/sources.list.d/ceph.list ]; then rm -rf /etc/apt/sources.list.d/ceph.list; fi;
+# if [ -e /etc/apt/sources.list.d/pve-enterprise.list ]; then rm -rf /etc/apt/sources.list.d/pve-enterprise.list; fi;
+# if [ -e /etc/apt/sources.list.d/ceph.list ]; then rm -rf /etc/apt/sources.list.d/ceph.list; fi;
 # Is this better?
 echo "Waiting to install OSX-PROXMOX..."
 echo " "
 
 apt update > /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 
-if [ $? -ne 0 ]
-then 
-	echo " "
-	echo "Error with 'apt-get update' ..."
-	echo "Trying to change /etc/apt/sources.list"
-	echo " "
-	# Always using a Brazilian server will not be fast...
- 	# I suggest using the users home country, As it will always be faster.
- 	Country=$(curl -s https://ipinfo.io/country | tr '[:upper:]' '[:lower:]')
-	sed -i "s/ftp.$Country.debian.org/ftp.debian.org/g" /etc/apt/sources.list
-		
-	echo "Retrying 'apt-get update' ..."
-	echo " "
-
-	apt-get update >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
-	
-	if [ $? -ne 0 ]; then echo "Error with 'apt-get update' ..."; exit; fi		
-fi
+# if [ $? -ne 0 ]
+# then 
+# 	echo " "
+# 	echo "Error with 'apt-get update' ..."
+# 	echo "Trying to change /etc/apt/sources.list"
+# 	echo " "
+# 	# Always using a Brazilian server will not be fast...
+#  	# I suggest using the users home country, As it will always be faster.
+#  	Country=$(curl -s https://ipinfo.io/country | tr '[:upper:]' '[:lower:]')
+# 	sed -i "s/ftp.$Country.debian.org/ftp.debian.org/g" /etc/apt/sources.list
+# 		
+# 	echo "Retrying 'apt-get update' ..."
+# 	echo " "
+# 
+# 	apt-get update >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
+# 	
+# 	if [ $? -ne 0 ]; then echo "Error with 'apt-get update' ..."; exit; fi		
+# fi
 
 apt install git -y >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 
-git clone https://github.com/luchina-gabriel/OSX-PROXMOX.git >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
+git clone https://github.com/Jberlinsky/OSX-PROXMOX.git >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 
 if [ ! -e /root/OSX-PROXMOX ]; then mkdir -p /root/OSX-PROXMOX; fi;
 
